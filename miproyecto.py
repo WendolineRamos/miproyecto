5ec2e7d13da9d5950e6611ba680c50ff8616d62c
WendolineRamos <wendoline.ramos@alumnos.udg.mx>
2016-12-08 17:54:16 -0600

    agregue clase Imagen

---
+++ miproyecto.py
@@ -0,0 +1,19 @@
+class Imagen():
+    def tamarindo (self):
+        print ("1,2 y 3... tamarindo")
+    def escala_grises (self):
+        pass
+    def binarizacion (self):
+        pass
+class Sonido():
+    def reproducir (self):
+        print ("Reproduciendo...")
+    def volumen (self):
+        pass
+    def fourier (self):
+        pass
+class git():
+    def push (self):
+        pass
+    def add (self):
+        pass
